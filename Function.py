98962bf76ad4abd6b00035a7a6946a6f0692b8d1
Kerdon <kerdon.laubert@voco.ee>
2024-11-20 15:23:15 +0200

    Progre Yl

---
+++ Function.py
@@ -0,0 +1,27 @@
+"""Function examples."""
+
+def func():
+    print("I´m inside the function")
+func()
+
+
+# my_name_is(name)
+name = "Mari"
+def my_name_is(name):
+    print(f"My name is {name}")
+
+    
+
+# sum_six(num)`
+def sum_six(num):
+    return num + 6
+
+# sum_numbers()
+def sum_numbers(a, b):
+    return int(a + b)
+print(sum_numbers(4234, 8127))
+
+# usd_to_eur()
+def usd_to_eur(usd):
+    return usd * 0.8
+print(usd_to_eur(100))
---
+++ String.py
@@ -0,0 +1,23 @@
+# Write your code here
+first_name = "James"
+last_name = "Bond"
+full_name = (first_name +" " +last_name)
+
+self_description_sentence = f"My name is {last_name}, {first_name} {last_name}."
+
+
+cake = "vahukoormarjadtäidispõhi"
+print(cake[0:8] + "\n" + cake[8:14] + "\n" + cake[14:20] + "\n" + cake[20:24])
+
+
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+
+
+
+every_other = original_string[::2]
+
+
+first_word = original_string.split()[0]
+first_word_reversed = first_word[::-1]
+
